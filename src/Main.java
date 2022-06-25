951c89d8eb23f9d555556926842e14683c5b93b8
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-06-25 11:27:22 -0700

    fix

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        HashMap<String, String> records = new HashMap<String, String>()
+        HashMap<String, String> records = new HashMap<String, String>();
         int menuOption = 0;
 
         while (menuOption != 6) {
